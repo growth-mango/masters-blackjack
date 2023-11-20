3a230f15310a1fb7eb56a8fe21392f0bb1d29c99
mango <esudatcom@gmail.com>
2023-11-20 22:55:27 +0900

    feat: Game 횟수 출력하는 gamePring 메서드 구현

--- src/main/java/blackjack/Step1.java
+++ src/main/java/blackjack/Step1.java
@@ -1,7 +1,18 @@
 package blackjack;
 
 public class Step1 {
+    private static final String GAME_PREFIX = "Game ";
+    private static int game = 1;
+
     public static void main(String[] args) {
-        System.out.println("간단 카드 게임을 시작합니다.");
+        System.out.println("간단 카드 게임을 시작합니다.\n");
+
+
+        gamePrint();
+    }
+
+    public static void gamePrint() {
+        System.out.println(GAME_PREFIX + game);
+        game++;
     }
 }
