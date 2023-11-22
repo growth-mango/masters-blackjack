15a98a5ffae3b2f585335c1b5e1b33f6a1c9d0d3
mango <esudatcom@gmail.com>
2023-11-22 18:45:53 +0900

    feat: 게임 횟수 출력하는 gamePrint 메서드 구현

--- src/main/java/blackjack/Step2.java
+++ src/main/java/blackjack/Step2.java
@@ -1,4 +1,21 @@
 package blackjack;
 
 public class Step2 {
+    private static final String GAME_PREFIX = "Game ";
+    private static int game = 1;
+    private static final String PROPERTY_PREFIX = "현재 재산: ";
+    private static final String BET_PREFIX = "얼마를 거시겠습니까? ";
+
+    public static void main(String[] args) {
+        System.out.println("간단 카드 게임을 시작합니다.");
+        System.out.println(PROPERTY_PREFIX);
+        System.out.println(BET_PREFIX);
+        gamePrint();
+
+    }
+
+    public static void gamePrint() {
+        System.out.println(GAME_PREFIX + game);
+        game++;
+    }
 }
